d5cceb1548d20b0f1ed4a466bd17c32899a16f53
Trong V <s117509@ap.be>
2025-10-03 11:04:24 +0000

    completed alien

--- Labo1/basics-alien/src/App.tsx
+++ Labo1/basics-alien/src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const letters : string[] = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+]
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Alien Alphabeth</h1>
+        {letters.map(letter => (
+            <button><img src={`https://raw.githubusercontent.com/slimmii/alien-alphabet/master/${letter}.png`} alt={letter} /></button>
+        ))}
     </>
   )
 }
