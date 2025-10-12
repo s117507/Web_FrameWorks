3d7a0a259ad58dd1ed9d3ff591b6ea1a75665fcb
Trong V <s117509@ap.be>
2025-10-12 15:00:49 +0000

    finished components

--- Labo2/components/src/App.tsx
+++ Labo2/components/src/App.tsx
@@ -1,33 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+interface HeaderProps {
+    title : string;
+    subtitle: string;
+}
+
+const Header = ({title, subtitle} : HeaderProps) => {
+    return (
+        <>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        </>
+    );
+};
+
+interface ListItemProps {
+    text: string;
+}
+
+const ListItem = ({text} : ListItemProps) => {
+    return <li>{text}</li>
+}
+
+interface ListProps {
+    items: string[];
+}
+
+const List = ({items}: ListProps) => {
+    return (
+        <>
+            <ul>
+                {items.map((item) =>
+                    <ListItem key={item} text={item} />
+
+                )}
+            </ul>
+        </>
+    );
+};
+
+interface FooterProps {
+    copy: string;
+    year: string;
+}
+
+const Footer = ({copy, year}: FooterProps) => {
+    return (
+        <>
+            <p>&#169; {copy} {year}</p>
+        </>
+    )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    let items : string[] = ["Item 1", "Item 2", "Item 3"]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Header title="Labo 2" subtitle="Components"/>
+        <List items={items}/>
+        <Footer copy="Trong Vo" year="2025"/>
     </>
   )
 }
